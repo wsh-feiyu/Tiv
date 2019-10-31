ed4fdd4b975217335da7fa9744a01e59fa4ee4c8
feiyu <feiyu>
2019-10-31 12:28:02 +0800

    更换 dkvideoplayer，但是kotlin无法继承videoview等，造成没有控制视图

--- app/src/main/java/tt/tiv/ui/activity/LivePlayActivity.kt
+++ app/src/main/java/tt/tiv/ui/activity/LivePlayActivity.kt
@@ -2,36 +2,40 @@ package tt.tiv.ui.activity
 
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
-import cn.jzvd.Jzvd
+import com.dueeeke.videoplayer.ijk.IjkPlayerFactory
 import kotlinx.android.synthetic.main.activity_liveplay.*
 import tt.tiv.R
 import tt.tiv.base.BaseActivity
-import tt.tiv.utils.JZMediaIjk
 
 class LivePlayActivity:BaseActivity() {
+
     override fun getLayoutId(): Int {
         return R.layout.activity_liveplay
     }
 
     override fun initData() {
         val url=intent.getStringExtra("url")
-        Jzvd.releaseAllVideos()
-        jzplayer.fullscreenButton.isGone=true
-        jzplayer.backButton.isGone=true
-        jzplayer.tinyBackImageView.isGone=true
-        jzplayer.setUp(url,null,Jzvd.SCREEN_FULLSCREEN,JZMediaIjk::class.java)
-        jzplayer.startVideo()
+        dkplayer.setUrl(url)
+//        dkplayer.setPlayerFactory(IjkPlayerFactory.create())
+        dkplayer.start()
+
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Jzvd.releaseAllVideos()
+        dkplayer.release()
     }
 
     override fun onPause() {
         super.onPause()
-        Jzvd.releaseAllVideos()
+        dkplayer.pause()
     }
 
+    override fun onBackPressed() {
+        if (!dkplayer.onBackPressed()){
+            super.onBackPressed()
+        }
+    }
 
 }
